30b6098cd557b11baa2379c2cea5b1db9df8fff9
Reiner Leal <reinerleal@gmail.com>
2017-10-01 21:37:51 -0700

    Learning

---
+++ src/playground/destructuring.js
@@ -0,0 +1,38 @@
+// Object Destructuring
+
+// const person = {
+//   name: 'Reiner',
+//   age: 33,
+//   location: {
+//     city: 'Phoenix',
+//     temp: 87
+//   }
+// };
+//
+// const { name = 'Anonymous', age } = person;
+//
+// console.log(`${name} is ${age}.`)
+//
+// const { city, temp: temperature = 90 } = person.location;
+//
+// console.log(`It's ${temperature} in ${city}.`);
+
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
+//
+// const { name: publisherName = 'Self-Published' } = book.publisher;
+//
+// console.log(publisherName);
+
+// Array Destructuring
+
+const address = ['2556 E Taro Ln', 'Phoenix', 'Arizona', '85050'];
+
+const [ street, city, state, zip ] = address;
+
+console.log(`${street}\n${city}, ${state} ${zip}`)
